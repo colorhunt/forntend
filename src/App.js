1d33bd7e6f0a6dc3c7f3851f6d834b7bd0aded20
ankita <nitin.syndell@gmail.com>
2023-06-22 19:14:17 +0530

    Updated code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+  
+  const [state, setState] = useState(0);
+  
+
+  useEffect(() => {
+    // const url = 'http://localhost/colorHuntApi/public/api/getoutwardchallen/12'
+    const url = process.env.REACT_APP_API_URL + '/getoutwardchallen/12'
+  
+    console.log(url);
+
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Error: ' + response.status);
+        }
+      })
+      .then(data => {
+        setState(data)
+        // Handle the API response data here
+        console.log(data);
+      })
+      .catch(error => {
+        // Handle any errors that occurred during the API call
+        console.error('Error: ', error);
+      });
+  });
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+
     </div>
   );
 }
